dbd5c7ce87b904177210009889de18c000aee043
AbhijitL <labhijitsingh13@gmail.com>
2019-06-23 14:40:43 +0530

    labels added

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,8 +29,10 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'raccoon':
+    if row_label == 'kok':
         return 1
+    elif row_label == 'sum':
+        return 2
     else:
         None
 
